7b920a8a75928b5066aea1b8d8c6ba10c3cbf528
Roland Rytz <roland.rytz@gmail.com>
2017-05-08 18:05:25 +0200

    Implement swimming jellyfish

--- aquarium.js
+++ aquarium.js
@@ -28,53 +28,59 @@ canvas.height = container.clientHeight;
 var mouseX = canvas.width/2;
 var mouseY = canvas.height/2;
 
-var draggyLine = new DraggyLine();
+/*var draggyLine = new DraggyLine(
+	 canvas.width/2
+	,canvas.height/2
+	,canvas.width/2+100
+	,canvas.height/2+100
+	,10
+);*/
 //draggyLine.render(Date.now());
 
-renderLoop();
+//renderLoop();
 
-window.addEventListener("mousemove", function(e){
+/*window.addEventListener("mousemove", function(e){
 	mouseX = e.clientX - container.offsetLeft;
 	mouseY = e.clientY - container.offsetTop;
-});
+});*/
 
-function renderLoop(){
+/*function renderLoop(){
 	context.clearRect(0, 0, canvas.width, canvas.height);
-	draggyLine.render(Date.now());
+	draggyLine.render(mouseX, mouseY);
 	requestAnimationFrame(renderLoop);
-}
+}*/
 
-function DraggyLine(){
+function DraggyLine(x, y, toX, toY, sections){
 
 	var points = [];
-	var pointCount = 500;
-	var targetLength = 2;
+	var pointCount = sections;
+	var totalLength = Math.sqrt(Math.pow(x - toX, 2) + Math.pow(y - toY, 2));
+	var sectionLength = totalLength/pointCount;
 
 	for(var i = 0; i <= pointCount; i++){
 		points.push([
-			 canvas.width/2
-			,canvas.height/2+i*targetLength
+			 x + (toX-x)*i/pointCount
+			,y + (toY-y)*i/pointCount
 		]);
 	}
 
-	function render(){
-		context.strokeStyle = "rgba(255, 255, 255, 0.8)";
+	function render(newX, newY){
 		context.beginPath();
 
 		var newPoints = [];
+
+		newPoints.push([newX, newY]);
 		
 		var i = 0;
 		context.moveTo(
-			 mouseX
-			,mouseY
+			 newPoints[i][0]
+			,newPoints[i][1]
 		);
-
-		newPoints.push([mouseX, mouseY]);
 
 		for(var i = 1; i <= pointCount; i++){
 
 			var currentDist = Math.sqrt(Math.pow(points[i][0] - newPoints[i-1][0], 2) + Math.pow(points[i][1] - newPoints[i-1][1], 2));
-			var relative = targetLength/currentDist;
+			var relative = sectionLength/currentDist;
 
 			newPoints.push([
 				 newPoints[i-1][0] + (points[i][0] - newPoints[i-1][0])*relative
@@ -98,58 +104,121 @@ function DraggyLine(){
 	
 }
 
-/*var jellyfish = new Jellyfish();
+var jellyfishCount = 20;
+var jellies = [];
+
+for(var i = 0; i < jellyfishCount; i++){
+	jellies.push(
+		new Jellyfish(
+			 canvas.width*Math.random()
+			,canvas.height*Math.random()
+			,Math.random()*Math.PI - Math.PI/2
+			,Math.random()+0.1
+		)
+	);
+}
 renderLoop();
 
 function renderLoop(){
 	context.clearRect(0, 0, canvas.width, canvas.height);
-	jellyfish.render(Date.now());
+	for(var i = 0; i < jellies.length; i++){
+		jellies[i].render(Date.now());
+	}
 	requestAnimationFrame(renderLoop);
 }
 
-function Jellyfish(){
+function Jellyfish(startX, startY, direction, speed){
 
-	var x = canvas.width/2;
-	var y = canvas.height/2;
+	var x = startX;
+	var y = startY;
 
-	var size = 100;
+	var size = 5 + Math.random()*20;
 
-	var angleSpan = 1.2*Math.PI;
+	var angleSpan = 1.5*Math.PI;
 
-	var pointCount = 10;
+	speed = (1/speed)*(1000/(2*Math.PI));
+
+	var draggyLineCount = 11;
+	var draggyLineLength = size*2;
+
+	var pointCount = 30;
 
 	var points = [];
+
+	var draggyLines = [];
 	
 	for(var i = 0; i <= pointCount; i++){
 		var p = (i / pointCount) * angleSpan - angleSpan/2 + Math.PI/2;
 		
-		points.push([Math.cos(p), Math.sin(p)]);
+		points.push([
+			 Math.cos(p)*size
+			,Math.sin(p)*size
+		]);
 	}
 
-	console.table(points);
+	for(var i = 0; i <= draggyLineCount; i++){
+		var time = Date.now();
+		var lineX = getCurrentX(points.length-1, time) - (getCurrentX(points.length-1, time) - getCurrentX(0, time))*(i/draggyLineCount);
+		draggyLines.push(
+			new DraggyLine(
+				 lineX
+				,getCurrentY(points.length-1, time)
+				,lineX
+				,getCurrentY(points.length-1, time)+draggyLineLength+Math.random()*(draggyLineLength*0.3)
+				,10
+			)
+		);
+	}
+
+	function getCurrentX(i, time){
+		return x + points[i][0] + points[i][0]*(1-points[i][1]/size)*(1-Math.sin(time/speed - 1*(1-points[i][1]/size))/2);
+	}
+
+	function getCurrentY(i, time){
+		return y - points[i][1]*(1+Math.sin(time/speed)*0.3);
+	}
 
 	function render(time){
 
-		var xFactor = Math.sin(time/1000)/2;
-		//var xFactor = 1;
-		var yFactor = 1-Math.sin(time/1000)/2;context.strokeStyle = "rgba(255, 255, 255, 0.8)";
+		y -= (size)*(0.3+Math.sin(time/speed+0.1)/2)/(speed/20);
+		
+		context.strokeStyle = "rgba(255, 255, 255, 0.2)";
+		context.fillStyle = "rgba(150, 150, 150, 0.3)";
+
+		context.save();
+		context.translate(startX, startY);
+		context.rotate(direction);
+		context.translate(-startX, -startY);
+
+		for(var i = 0; i <= draggyLineCount; i++){
+			var lineX = getCurrentX(points.length-1, time) - (getCurrentX(points.length-1, time) - getCurrentX(0, time))*(Math.cos(Math.PI*i/draggyLineCount)/2+0.5);
+			draggyLines[i].render(
+				 lineX+Math.random()
+				,getCurrentY(points.length-1, time)
+			);
+		}
+
+		context.strokeStyle = "rgba(255, 255, 255, 0.6)";
+		
 		context.beginPath();
+		var i = 0;
 		context.moveTo(
-			 x + points[0][0]*size + points[0][0]*(1-points[0][1])*xFactor*size
-			,y - points[0][1]*size
+			 getCurrentX(i, time)
+			,getCurrentY(i, time)
 		);
 
 		for(var i = 1; i <= pointCount; i++){
 			context.lineTo(
-				 x + points[i][0]*size + points[i][0]*(1-points[i][1])*xFactor*size
-				,y - points[i][1]*size
+				 getCurrentX(i, time)
+				,getCurrentY(i, time)
 			);
 		}
 		context.closePath();
 
+		context.fill();
 		context.stroke();
 
-		
+		context.restore();	
 		
 	}
 
@@ -159,7 +228,7 @@ function Jellyfish(){
 	
 }
 
-*/
+
 
 /*generateStemPlant();
 
